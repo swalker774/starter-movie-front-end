4405798dd1b02e33d8c75b964b84b708fdf8fc93
Stephen Walker <35walke774@gmail.com>
2024-02-19 13:19:00 -0500

    updated base url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,6 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+  process.env.REACT_APP_API_BASE_URL ||
+  "https://welovemoviesback-mdek.onrender.com";
 
 // const {
 //   NODE_ENV = "development",
